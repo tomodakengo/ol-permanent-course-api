b19a4e3694d287a5884853e8ff27470818ae993f
akarisan <joa.tomoda@gmail.com>
2022-06-05 10:06:13 +0900

    Make apicontroller

---
+++ olpc-api/app/Http/Controllers/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Get all data
+    public function getAllCourse() {
+
+    }
+    
+    // Get data with selected id
+    public function getCourse() {
+        
+    }
+}
