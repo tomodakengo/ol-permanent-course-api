76f39dd7bc1288f43ca8670af6b6fd9e3e04a064
akarisan <joa.tomoda@gmail.com>
2023-04-11 20:49:06 +0900

    Fix detail infomation

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -10,30 +10,30 @@
         <div class="card-body row">
             <div class="col-md-4">
                 <h5 class="card-title">{{ $details->name }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">北海道</h6>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $details->prefecture }}</h6>
             </div>
 
             <div class="col-md-8 table-responsive">
                 <table class="table table-striped">
                     <tr>
                         <th>都道府県管理番号</th>
-                        <td>10</td>
+                        <td>{{ $details->managementNo }}</td>
                     </tr>
                     <tr>
                         <th>公認コース管理番号</th>
-                        <td>120</td>
+                        <td>{{ $details->licenseNo }}</td>
                     </tr>
                     <tr>
                         <th>スタート地点</th>
-                        <td>札幌駅南口</td>
+                        <td>{{ $details->startPosition }}</td>
                     </tr>
                     <tr>
                         <th>地図取扱所</th>
-                        <td>札幌駅構内観光センター</td>
+                        <td>{{ $details->whereToGet }}</td>
                     </tr>
                     <tr>
                         <th>コース距離</th>
-                        <td>10km</td>
+                        <td>{{ $details->distance }}</td>
                     </tr>
                 </table>
             </div>
